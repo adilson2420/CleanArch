781214c0fa2749438e55259e6e6216274777c9cd
Adilson Cambamba <adilson.cambamba@kiamisoft.ao>
2024-12-04 22:27:33 +0100

    AppDbContext

---
+++ CleanArch.Infrastutura/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Dominio.Entidades;
+using CleanArch.Infrastutura.EntityConfiguration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infrastutura.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Membro> Membros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ConfiguracaoMembro());
+        }
+    }
+}
---
+++ CleanArch.Infrastutura/EntityConfiguration/ConfiguracaoMembro.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArch.Infrastutura.EntityConfiguration
+{
+    public class ConfiguracaoMembro : IEntityTypeConfiguration<Membro>
+    {
+        public void Configure(EntityTypeBuilder<Membro> builder)
+        {
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.Nome).HasMaxLength(100).IsRequired();
+            builder.Property(m => m.Email).HasMaxLength(100).IsRequired();
+            builder.Property(m => m.Sexo).HasMaxLength(1);
+            builder.Property(m => m.Activo).IsRequired();
+
+            builder.HasData(
+                new Membro(1, "Adilson", "adilson@gmail.com", 'M', true),
+                new Membro(1, "Clara", "clara@gmail.com", 'F', true)
+                );
+        }
+    }
+}
