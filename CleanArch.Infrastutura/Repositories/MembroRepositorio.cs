69d4cb634ea6679054c830ab03dafb269babee05
Adilson Cambamba <adilson.cambamba@kiamisoft.ao>
2024-12-04 22:40:45 +0100

    implementação da infrastutura

---
+++ CleanArch.Infrastutura/Repositories/MembroRepositorio.cs
@@ -0,0 +1,61 @@
+﻿using CleanArch.Dominio.Abstracoes;
+using CleanArch.Dominio.Entidades;
+using CleanArch.Infrastutura.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infrastutura.Repositories
+{
+    internal class MembroRepositorio : IMembroRepositorio
+    {
+        protected readonly AppDbContext _db;
+
+        public MembroRepositorio(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Membro> AdicionarMembro(Membro membro)
+        {
+            if (membro is null)
+                throw new ArgumentNullException(nameof(membro));
+            await _db.Membros.AddAsync(membro);
+            return membro;
+        }
+
+        public void Atualizar(Membro membro)
+        {
+            if (membro is null)
+                throw new ArgumentNullException(nameof(membro));
+            _db.Membros.Update(membro);
+        }
+
+        public async Task<Membro> BuscarMembroPorId(int idMembro)
+        {
+            var membro = await _db.Membros.FindAsync(idMembro);
+            if (membro is null)
+                throw new InvalidOperationException("Membro não encontrado");
+            return membro;
+        }
+
+        public async Task<IEnumerable<Membro>> BuscarMembros()
+        {
+            var membros = await _db.Membros.ToListAsync();
+            return membros ?? Enumerable.Empty<Membro>();
+        }
+
+        public async Task<Membro> EliminarMembro(int idMembro)
+        {
+            var membro = await BuscarMembroPorId(idMembro);
+            if (membro is null)
+                throw new InvalidOperationException("membro não encontrado");
+
+            _db.Membros.Remove(membro);
+            return membro;
+        }
+    }
+}
---
+++ CleanArch.Infrastutura/Repositories/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using CleanArch.Dominio.Abstracoes;
+using CleanArch.Infrastutura.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infrastutura.Repositories
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private IMembroRepositorio? _membroRepo;
+        private readonly AppDbContext _db;
+
+        public UnitOfWork(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IMembroRepositorio MembroRepositorio { get { return _membroRepo = _membroRepo ?? new MembroRepositorio(_db); } }
+
+        public async Task CommitAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
