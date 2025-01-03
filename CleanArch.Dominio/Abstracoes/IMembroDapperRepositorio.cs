04dd6f0deeb48550fc32ec716b4041519fce5797
Adilson Cambamba <adilson.cambamba@kiamisoft.ao>
2025-01-03 17:51:56 +0100

    Implementação do Dapper e criação da interface e o repositorio

---
+++ CleanArch.Dominio/Abstracoes/IMembroDapperRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using CleanArch.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Dominio.Abstracoes
+{
+    public interface IMembroDapperRepositorio
+    {
+        Task<IEnumerable<Membro>> Membros();
+        Task<Membro> MembroPorId(int id);
+    }
+}
---
+++ CleanArch.Infrastutura/Repositories/MembroDapperRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using CleanArch.Dominio.Abstracoes;
+using CleanArch.Dominio.Entidades;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infrastutura.Repositories
+{
+    public class MembroDapperRepositorio : IMembroDapperRepositorio
+    {
+        private readonly IDbConnection _dbConnection;
+        public MembroDapperRepositorio(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public async Task<Membro> MembroPorId(int id)
+        {
+            string query = "SELECT * FROM Membros";
+            return await _dbConnection.QueryFirstOrDefaultAsync<Membro>(query, new { Id = id });
+        }
+
+        public async Task<IEnumerable<Membro>> Membros()
+        {
+            string query = "SELECT * FROM Membros";
+            return await _dbConnection.QueryAsync<Membro>(query);
+        }
+    }
+}
